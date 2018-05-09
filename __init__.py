ecae6736de9e70392a86cb26d9e7769df141285c
John Lawrence <jalawrence6023@gmail.com>
2018-05-09 12:58:08 -0400

    pylint

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
+"""This module provides URL signing and checking functionality"""
 from .signer import Signer
--- signer.py
+++ signer.py
@@ -1,18 +1,20 @@
+"""URL signing/checking functionality"""
+# pylint: disable=too-many-arguments,too-many-locals
 from random import randint
 import urllib.parse
 import hashlib
 import json
 import time
 import hmac
-import sys
 import base64
 
 try:
   from flask import request
-except:
+except: # pylint: disable=bare-except
   pass
 
 class Signer:
+  """All behavior required to assign signatures to urls based on public/private keys"""
   def __init__(self, public_key, private_key):
     self.public_key = public_key
     self.private_key = None
@@ -20,16 +22,18 @@ class Signer:
     if callable(private_key):
       try:
         self.private_key = private_key(public_key)
-      except:
+      except: # pylint: disable=bare-except
         self.private_key = None
 
     else:
       self.private_key = private_key
 
   def get_signed_url(self, method, url, body, headers):
+    """Calculate the signature and return the signed url"""
     return self.get_signature(method, url, body, headers)['s-url']
 
   def get_signing_errors(self, method, url, body, headers):
+    """Calculate the signature and compare to the given signature, returning errors if needed"""
     parsed_url = urllib.parse.urlparse(url)
     parsed_query = urllib.parse.parse_qs(parsed_url.query)
     signature = self.get_signature( \
@@ -62,13 +66,11 @@ class Signer:
       now = int(time.time())
       if abs(now - signed_time) > 300:
         signing_errors.append('Signing timestamp too old')
-    except Exception as e:
+    except Exception: # pylint: disable=broad-except
       signing_errors.append('Invalid signing timestamp')
 
     if signature.get('s-nonce') is None:
       signing_errors.append('Missing nonce')
-      # TODO - check if the nonce value has changed.  Use redis & cache
-      # for 5 minutes to verify.  Key should be public key + nonce
 
     if parsed_query.get('s-signature', ['missing'])[0] != signature.get('s-signature'):
       signing_errors.append('Signature does not match')
@@ -76,36 +78,43 @@ class Signer:
     return signing_errors
 
   def get_request_signing_errors(self):
-    return self.get_signing_errors(request.method.lower(), request.url, request.get_data(), request.headers)
+    """Check if the current request (in context) has been properly signed"""
+    return self.get_signing_errors(
+        request.method.lower(),
+        request.url,
+        request.get_data(),
+        request.headers)
 
   def get_signature(self, method, url, body, headers, forced_time=None, forced_nonce=None):
-    md = hashlib.md5()
+    """Calculate a request signature based on given context"""
+    # pylint: disable=too-many-branches,unused-argument
+    hasher = hashlib.md5()
 
     if isinstance(body, (bytes, bytearray)):
-      md.update(body)
+      hasher.update(body)
     elif isinstance(body, dict):
-      md.update(json.dumps(body).encode('utf-8'))
+      hasher.update(json.dumps(body).encode('utf-8'))
     elif body is None:
-      md.update(''.encode('utf-8'))
+      hasher.update(''.encode('utf-8'))
     else:
-      md.update(body.encode('utf-8'))
+      hasher.update(body.encode('utf-8'))
 
-    body_md5 = md.hexdigest()
+    body_md5 = hasher.hexdigest()
 
     parsed_url = urllib.parse.urlparse(url)
     parsed_query = urllib.parse.parse_qs(parsed_url.query)
 
     # For the sake of checking a signature, remove these calculated
     # parameters from the existing query string
-    for p in ['s-key', 's-time', 's-hash', 's-signature', 's-nonce']:
-      if p in parsed_query:
-        del parsed_query[p]
+    for parm in ['s-key', 's-time', 's-hash', 's-signature', 's-nonce']:
+      if parm in parsed_query:
+        del parsed_query[parm]
 
     additional_query = {
-      's-key': self.public_key,
-      's-time': str(int(time.time())),
-      's-hash': body_md5,
-      's-nonce': randint(0, 1000000000),
+        's-key': self.public_key,
+        's-time': str(int(time.time())),
+        's-hash': body_md5,
+        's-nonce': randint(0, 1000000000),
     }
 
     # For the sake of checking a signature, allow the time & none value to be
@@ -117,29 +126,36 @@ class Signer:
       additional_query['s-nonce'] = forced_nonce
 
     full_parms = {}
-    for p in parsed_query:
-      full_parms[p] = ','.join(parsed_query[p])
+    for parm in parsed_query:
+      full_parms[parm] = ','.join(parsed_query[parm])
 
-    for p in additional_query:
-      full_parms[p] = additional_query[p]
+    for parm in additional_query:
+      full_parms[parm] = additional_query[parm]
 
-    qs = []
-    for p in sorted(full_parms.keys()):
-      qs.append('{}={}'.format(p, full_parms[p]))
+    query_string = []
+    for parm in sorted(full_parms.keys()):
+      query_string.append('{}={}'.format(parm, full_parms[parm]))
 
     key = '{}\n{}\n{}\n{}'.format( \
         method.lower(),
         parsed_url.path.lower(),
-        '&'.join(qs),
+        '&'.join(query_string),
         additional_query['s-nonce'])
 
     del additional_query['s-hash']
 
-    signature = base64.b64encode(hmac.new(str(self.private_key).encode('utf-8'), msg=key.encode('utf-8'), digestmod=hashlib.sha256).digest()).decode("utf-8")
+    signature = base64.b64encode(
+        hmac.new(
+            str(self.private_key).encode('utf-8'),
+            msg=key.encode('utf-8'),
+            digestmod=hashlib.sha256).digest()
+        ).decode("utf-8")
 
     additional_query['s-signature'] = signature
 
-    additional_query_string = '&'.join([k + '=' + urllib.parse.quote_plus(str(additional_query[k])) for k in additional_query])
+    additional_query_string = '&'.join(
+        [k + '=' + urllib.parse.quote_plus(str(additional_query[k])) for k in additional_query])
+
     if '?' in url:
       signed_url = url + '&' + additional_query_string
     else:
@@ -148,4 +164,3 @@ class Signer:
     additional_query['s-url'] = signed_url
     additional_query['s-hash'] = body_md5
     return additional_query
-
