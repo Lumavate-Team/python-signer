94248413119193b75f2b7fdb5eb4573293e58f6a
Eric Seidel <e.seidel@lumavate.com>
2020-11-05 14:18:22 -0500

    changed to use single key which is all blake2b needs

--- value_hasher.py
+++ value_hasher.py
@@ -5,18 +5,8 @@ from hmac import compare_digest
 class ValueHasher:
   AUTH_SIZE = 16
 
-  def __init__(self, public_key, private_key):
-    self.public_key = public_key
-    self.private_key = None
-
-    if callable(private_key):
-      try:
-        self.private_key = private_key(public_key)
-      except: # pylint: disable=bare-except
-        self.private_key = None
-
-    else:
-      self.private_key = private_key
+  def __init__(self, private_key):
+    self.private_key = private_key
 
   def sign(self, value):
     h = blake2b(digest_size=self.AUTH_SIZE, key=self.private_key.encode('utf-8'))
